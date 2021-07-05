1ca5d06ed664af6450d2d2c2ff9c5570d7fb989c
Jonathan <jonmohr@outlook.com>
2021-07-05 16:32:41 -0400

    react app created

---
+++ jd-frontend/jays-designs/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Router, Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
