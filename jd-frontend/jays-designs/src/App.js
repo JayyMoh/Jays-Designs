cc3f7e474defb7e1d559b664522b131bf8c4f13f
Jonathan <jonmohr@outlook.com>
2021-07-05 16:41:51 -0400

    Route to contact component created

--- jd-frontend/jays-designs/src/App.js
+++ jd-frontend/jays-designs/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Home from './Components/Home' 
+import Contact from './Components/Contact'
 import { Route, Link } from 'react-router-dom'
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path='/' component={ Home } />
+      <Route exact path='/contact' component={ Contact } />
     </div>
   );
 }
