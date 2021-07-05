716fbc37f2f532060306fd4c72d0a22ca7bae1bc
Jonathan <jonmohr@outlook.com>
2021-07-05 17:00:06 -0400

    NavBar links routing properly.

--- jd-frontend/jays-designs/src/Components/NavBar.jsx
+++ jd-frontend/jays-designs/src/Components/NavBar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
     return (
-        <div>
-            <h1>This is the NavBar</h1>
+        <div className='nav-container'>
+            <nav>
+                <ul className='nav-ul'>
+                    <li className='nav-li'>
+                        <Link to='/'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-li'>
+                        <Link to='/projects'>
+                            My Work
+                        </Link>
+                    </li>
+                    <li className='nav-li'>
+                        <Link to='/about'>
+                            About Me
+                        </Link>
+                    </li>
+                    <li className='nav-li'>
+                        <Link to='/shop'>
+                            Shop
+                        </Link>
+                    </li>
+                    <li className='nav-li'>
+                        <Link to='/contact'>
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
