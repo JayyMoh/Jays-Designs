bab2155b2ffd975e7eb0e4e0479f75c64533c0d3
Jonathan <jonmohr@outlook.com>
2021-07-06 23:36:11 -0400

    Added title to images on work page

--- jd-frontend/jays-designs/src/Components/Projects/Projects.jsx
+++ jd-frontend/jays-designs/src/Components/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ImageList, ImageListItem } from '@material-ui/core'
+import { ImageList, ImageListItem, ImageListItemBar } from '@material-ui/core'
 import itemData from './work'
 
 
@@ -11,11 +11,15 @@ function Projects() {
                 <ImageList sx={{ width: 500, height: 500 }} cols={3} rowHeight={300}>
                     {itemData.map((item) => (
                         <ImageListItem key={item.img}>
-                        <img
-                            srcSet={`${item.img}?w=161&fit=crop&auto=format 1x,
-                            ${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                        />
+                            <img
+                                srcSet={`${item.img}?w=161&fit=crop&auto=format 1x,
+                                ${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                                alt={item.title}
+                            />
+                            <ImageListItemBar
+                                title={item.title}
+                                postition="below"
+                            />
                         </ImageListItem>
                     ))}
                 </ImageList>
