9963efad400a9f8dfe759e9c3b06f816850551df
Jonathan <jonmohr@outlook.com>
2021-07-12 17:56:21 -0400

    Menu icon hidden

--- jd-frontend/jays-designs/src/Components/Navbar/Navbar.jsx
+++ jd-frontend/jays-designs/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, IconButton, Badge, Typography, useMediaQuery } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
+import MenuIcon from '@material-ui/icons/Menu'
 import { Link } from 'react-router-dom'
 
 import makeStyles from './styles'
+
 
 function Navbar({ totalItems }) {
 
@@ -13,11 +15,14 @@ function Navbar({ totalItems }) {
         <div>
             <AppBar position="fixed" className={ classes.appBar } color="inherit">
                 <Toolbar>
+                    <IconButton edge="start" className={ classes.menuButton } color="inherit" aria-label="menu" >
+                        {<MenuIcon />}
+                    </IconButton>
                     <Typography component={Link} to="/" className={ classes.title } color="inherit">
                         <img src="https://cdn.discordapp.com/attachments/822281280615219211/862024271659925514/jd.png" alt="Jays Designs" height="30px" className={ classes.image } />
                     </Typography>
-                    <ul className='nav-ul'>
-                        <li className='nav-li'>
+                    <ul className={ classes.navUl }>
+                        <li className={ classes.navLi }>
                             <Link className={ classes.Link } to='/'>
                                 Home
                             </Link>
--- jd-frontend/jays-designs/src/Components/Navbar/styles.js
+++ jd-frontend/jays-designs/src/Components/Navbar/styles.js
@@ -1,6 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { useMediaQuery } from '@material-ui/core';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
     appBar: {
         backgroundColor: 'black',
         boxShadow: 'none',
@@ -25,4 +26,25 @@ export default makeStyles(() => ({
     grow: {
         flexGrow: 1,
     },
+    menuButton: {
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        }
+    },
+    navUl: {
+        listStyleType: 'none',
+        display: 'flex',
+        justifyContent: 'center',      
+    },
+    navLi: {
+        float: 'left',
+        marginRight: '10px',
+        border: '2px solid black',
+        padding: '5px',
+        borderRadius: '5px',
+        color: 'rgb(0,255,105)',
+        textDecoration: 'none',      
+    }
 }));
+
+
